556d16be4ce10888f88048b42fe9a103a986d035
Ruan Araújo <ruands2002@gmail.com>
2022-07-31 23:07:15 -0300

    feat: melhorias no componente 'button' e add lógicas para adicionar números, operadores e limpar

--- src/components/Button.js
+++ src/components/Button.js
@@ -11,12 +11,28 @@ const styles = StyleSheet.create({
     borderWidth: 1,
     borderColor: "#888",
   },
+  operationButton: {
+    color: "#fff",
+    backgroundColor: "#fa8231",
+  },
+  buttonDouble: {
+    width: (Dimensions.get("window").width / 4) * 2,
+  },
+  buttonTriple: {
+    width: (Dimensions.get("window").width / 4) * 3,
+  },
 });
 
-export function Button({ label, onClick }) {
+export function Button({ label, double, triple, operation, onClick }) {
+  const stylesButton = [styles.button];
+
+  if (double) stylesButton.push(styles.buttonDouble);
+  if (triple) stylesButton.push(styles.buttonTriple);
+  if (operation) stylesButton.push(styles.operationButton);
+
   return (
-    <TouchableHighlight onPress={onClick}>
-      <Text style={styles.button}>{label}</Text>
+    <TouchableHighlight onPress={() => onClick(label)}>
+      <Text style={stylesButton}>{label}</Text>
     </TouchableHighlight>
   );
 }
